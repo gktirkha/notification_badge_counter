eae42adaff9b528d9fd2be635f4a92df9186f23a
gktirkha <gktirkha@gmail.com>
2026-05-15 09:21:04 +0530

    chore: add pigeon dependency and clean up pubspec comments

---
+++ pigeon/notification_badge_api.dart
@@ -0,0 +1,18 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(
+  PigeonOptions(
+    dartOut: "lib/src/pigeon/notification_badge_api.g.dart",
+    dartOptions: DartOptions(),
+    kotlinOut:
+        "android/src/main/kotlin/com/gtirkha/notification_badge/pigeon/NotificationBadgeApi.g.kt",
+    kotlinOptions: KotlinOptions(package: "com.gtirkha.notification_badge"),
+    swiftOut: 'ios/Classes/pigeon/NotificationBadgeApi.g.swift',
+    swiftOptions: SwiftOptions(),
+  ),
+)
+@HostApi()
+abstract class NotificationBadgeApi {
+  @async
+  bool setCount();
+}
